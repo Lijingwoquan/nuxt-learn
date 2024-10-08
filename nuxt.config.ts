1ef0067ea2a803b564969639bcfd873205cc9b1f
lzh <2115883273@qq.com>
2024-10-08 23:04:36 +0800

    引入element-plus tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  // 定义需要全局加载的文件。
+  modules: ["@element-plus/nuxt", "@nuxtjs/tailwindcss"],
+  css: ["element-plus/dist/index.css", "~/assets/css/main.css"],
+  build: {
+    // 生产环境使用 babel 转译依赖。
+    transpile: process.env.prod ? ["element-plus"] : [],
+  },
+  $production: {
+    routeRules: {
+      "/**": { isr: true },
+    },
+  },
+  $development: {
+    //
+  },
+  runtimeConfig: {
+    // 只在服务器端可用的私有键
+    apiSecret: "123",
+    // public中的键也可以在客户端使用
+    public: {
+      apiBase: "/api",
+    },
+  },
+  nitro: {},
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    vue: {
+      customElement: true,
+    },
+    vueJsx: {
+      mergeProps: true,
+    },
+  },
+  webpack: {
+    loaders: {
+      vue: {
+        hotReload: true,
+      },
+    },
+  },
+  vue: {
+    // 启用vue验性功能
+    propsDestructure: true,
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <!-- <Header /> -->
+        <el-icon>
+          <Fold />
+        </el-icon>
+        <el-button type="primary">Header</el-button>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+      <el-footer>
+        <!-- <Footer /> -->
+        <el-button type="primary">Footer</el-button>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
