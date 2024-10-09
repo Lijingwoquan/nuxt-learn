4837c7d835fccc93b76da912939422bffb0133f0
lzh <2115883273@qq.com>
2024-10-09 21:46:37 +0800

    seo属性动态设置

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,7 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: "orange",
-  middleware: "auth",
-  validate: async (route) => {
-    // 检查id是否由数字组成
-    
-    return true;
-  },
-});
-</script>
-
 <template>
   <div>
     <h1>关于页面</h1>
     <NuxtLink to="/">首页</NuxtLink>
   </div>
 </template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,24 @@
     <NuxtLink to="/about">关于页面</NuxtLink>
   </div>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "orange",
+  middleware: "auth",
+  validate: async (route) => {
+    return true;
+  },
+});
+
+const keywords = ref(["关键词1", "关键词2", "关键词3"]);
+const metaKeywords = computed(() => keywords.value.join(","));
+
+const title = ref("网站标题");
+const metaTitle = computed(() => title.value);
+
+useHead({
+  title: metaTitle,
+  meta: [{ name: "keywords", content: metaKeywords }],
+});
+</script>
